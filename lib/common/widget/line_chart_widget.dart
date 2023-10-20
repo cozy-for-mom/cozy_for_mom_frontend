2c286a979daab25c0c45e10d8f6be92359374f4e
ayoung0073 <ayong0310@naver.com>
2023-10-20 21:07:38 +0900

    feat: 스크롤 없는 차트 구현

---
+++ lib/common/widget/line_chart_widget.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+const Color activeColor = Color(0xff5CA6F8);
+const Color defaultColor = Color(0xffBADBFF);
+const Color thickLineColor = Color(0xffCDCED5);
+const Color defaultLineColor = Color(0xffF0F0F5);
+
+class LineChart extends StatelessWidget {
+  const LineChart({
+    super.key,
+    required this.dataList,
+    required this.baseValue,
+    required this.unit,
+  });
+
+  final List<LineChartData> dataList;
+  final double baseValue;
+  final String unit;
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      backgroundColor: Colors.white,
+      series: <ChartSeries>[
+        LineSeries<LineChartData, String>(
+          dataSource: dataList,
+          xValueMapper: (LineChartData data, _) => data.xValue,
+          yValueMapper: (LineChartData data, _) => data.yValue,
+          color: defaultColor,
+          width: 3,
+          markerSettings: const MarkerSettings(
+            isVisible: true,
+            shape: DataMarkerType.circle,
+            color: Colors.white,
+            borderColor: defaultColor,
+            borderWidth: 2,
+          ),
+        ),
+      ],
+      tooltipBehavior: TooltipBehavior(
+        color: const Color(0xff5CA6F8), // 툴팁 배경색 설정
+        // opacity: 1,
+        borderWidth: 2, // 툴팁 테두리 두께 설정
+        duration: 5000,
+        enable: true,
+        builder: (dynamic data, dynamic point, dynamic series, int pointIndex,
+            int seriesIndex) {
+          return Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.black.withOpacity(0),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  '${data.yValue} ',
+                  style: const TextStyle(color: Colors.white),
+                ),
+                Text(
+                  unit,
+                  style: const TextStyle(
+                    color: Color(0xffBADBFF),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      primaryXAxis: CategoryAxis(
+        majorGridLines: const MajorGridLines(
+          width: 0,
+        ),
+        majorTickLines: const MajorTickLines(
+          // 짧은 눈금선 제거
+          width: 0,
+        ),
+        minorGridLines: const MinorGridLines(
+          width: 0,
+        ),
+        axisLine: const AxisLine(
+          color: thickLineColor,
+          width: 1.7,
+        ),
+        labelStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
+      ),
+      primaryYAxis: NumericAxis(
+        interval: 5,
+        axisLine: const AxisLine(width: 1, color: Colors.white),
+        majorTickLines: const MajorTickLines(width: 0), // 제거
+        opposedPosition: true,
+        maximumLabels: 4,
+        minorTicksPerInterval: 1,
+        tickPosition: TickPosition.inside,
+        minorTickLines: const MinorTickLines(
+          size: 400,
+          width: 1.7,
+          color: defaultLineColor,
+        ),
+        majorGridLines: const MajorGridLines(
+          width: 1.7,
+          color: thickLineColor,
+        ),
+        minorGridLines: const MinorGridLines(
+          width: 1,
+        ),
+        minimum: baseValue,
+        plotBands: <PlotBand>[
+          PlotBand(
+            borderColor: thickLineColor,
+            color: thickLineColor,
+            isVisible: true,
+            borderWidth: 1.7,
+            start: baseValue,
+            end: baseValue,
+          ),
+          PlotBand(
+            borderColor: thickLineColor,
+            color: thickLineColor,
+            isVisible: true,
+            borderWidth: 2.5,
+            start: baseValue + 15, // TODO 어떻게 지정해야할지 고민 필요
+            end: baseValue + 15,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LineChartData {
+  final String xValue;
+  final double yValue;
+
+  LineChartData(this.xValue, this.yValue);
+}
