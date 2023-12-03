e5019734c7b021d93d9568ca7f65b11dddf6bbbe
ayoung0073 <ayong0310@naver.com>
2023-12-03 20:46:39 +0900

    fix: add screen postfix

--- lib/screen/tab/baby/baby_main_screen.dart
+++ lib/screen/tab/baby/baby_main_screen.dart
@@ -6,18 +6,18 @@ import 'package:cozy_for_mom_frontend/screen/tab/baby/custom_button.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: BabyMainPage(),
+    home: BabyMainScreen(),
   ));
 }
 
-class BabyMainPage extends StatefulWidget {
-  const BabyMainPage({super.key});
+class BabyMainScreen extends StatefulWidget {
+  const BabyMainScreen({super.key});
 
   @override
-  State<BabyMainPage> createState() => _BabyMainPageState();
+  State<BabyMainScreen> createState() => _BabyMainScreenState();
 }
 
-class _BabyMainPageState extends State<BabyMainPage> {
+class _BabyMainScreenState extends State<BabyMainScreen> {
   @override
   Widget build(BuildContext context) {
     final baby = BabyProfile(
