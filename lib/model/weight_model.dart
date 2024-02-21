c27bf69718a44162d87b85f6182cecfe17a81a4d
ck <70942824+ck-pp@users.noreply.github.com>
2024-02-22 03:17:04 +0900

    Add: 산모 체중 테스트 json 파일 및 모델, api service 생성

---
+++ lib/model/weight_model.dart
@@ -0,0 +1,19 @@
+class PregnantWeights {
+  List<PregnantWeight> weights;
+
+  PregnantWeights({required this.weights});
+}
+
+class PregnantWeight {
+  String dateTime;
+  double weight;
+
+  PregnantWeight({required this.dateTime, required this.weight});
+
+  factory PregnantWeight.fromJson(Map<String, dynamic> json) {
+    return PregnantWeight(
+      weight: json['weight'] as double,
+      dateTime: json['date'] as String,
+    );
+  }
+}
