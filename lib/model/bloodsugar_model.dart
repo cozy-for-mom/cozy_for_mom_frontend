f446e41e38636ff81e77bde5b61c3ca5ae6e3311
ck <70942824+ck-pp@users.noreply.github.com>
2024-03-05 02:15:05 +0900

    Add: 혈당 테스트 json 파일 및 모델 클래스 생성

---
+++ lib/model/bloodsugar_model.dart
@@ -0,0 +1,36 @@
+import 'dart:ffi';
+
+class PregnantBloosdugars {
+  List<PregnantBloosdugar> bloodsugars;
+
+  PregnantBloosdugars({required this.bloodsugars});
+}
+
+class PregnantBloosdugar {
+  int id;
+  String type;
+  int level;
+
+  PregnantBloosdugar(
+      {required this.id, required this.type, required this.level});
+
+  factory PregnantBloosdugar.fromJson(Map<String, dynamic> json) {
+    return PregnantBloosdugar(
+        id: json['id'] as int,
+        type: json['type'] as String,
+        level: json['level'] as int);
+  }
+}
+
+class BloodsugarPerPeriod {
+  String endDate;
+  double averageLevel;
+
+  BloodsugarPerPeriod({required this.endDate, required this.averageLevel});
+
+  factory BloodsugarPerPeriod.fromJson(Map<String, dynamic> json) {
+    return BloodsugarPerPeriod(
+        endDate: json['endDate'] as String,
+        averageLevel: json['averageLevel'] as double);
+  }
+}
