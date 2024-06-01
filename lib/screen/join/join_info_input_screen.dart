79b5b555cb5054efb022c11803d4476cc34f7bfc
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-01 20:59:22 +0900

    Feat: 회원가입 페이지 POST API 요청 테스트 완료

--- lib/screen/join/join_info_input_screen.dart
+++ lib/screen/join/join_info_input_screen.dart
@@ -7,26 +7,9 @@ import 'package:cozy_for_mom_frontend/screen/join/mom_nickname.dart';
 import 'package:cozy_for_mom_frontend/screen/join/join_input_data.dart';
 import 'package:cozy_for_mom_frontend/screen/join/baby_duedate.dart';
 import 'package:cozy_for_mom_frontend/screen/join/baby_fetal_info.dart';
-import 'package:cozy_for_mom_frontend/screen/join/baby_gender.dart';
-import 'package:cozy_for_mom_frontend/screen/join/baby_birthname.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const JoinInfoInputScreen(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.light(),
-        fontFamily: 'Pretendard',
-      ),
-    );
-  }
-}
-// TODO main.dart의 하위 페이지로 연동 후, 위의 코드 삭제
+import 'package:cozy_for_mom_frontend/screen/join/baby_gender_name.dart';
+import 'package:cozy_for_mom_frontend/model/user_model.dart';
+import 'package:cozy_for_mom_frontend/service/user/join_api_service.dart';
 
 class JoinInfoInputScreen extends StatefulWidget {
   const JoinInfoInputScreen({super.key});
@@ -37,107 +20,116 @@ class JoinInfoInputScreen extends StatefulWidget {
 
 class _JoinInfoInputScreenState extends State<JoinInfoInputScreen> {
   final PageController _pageController = PageController();
+  final joinApiService = JoinApiService();
   int _currentPage = 0;
-  final int _totalPage = 7;
-  // bool isButtonEnabled() {
-  //   switch (_currentPage) {
-  //     case 0:
-  //       return
-  //   }
-  // }
+  final int _totalPage = 6;
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    return ChangeNotifierProvider(
-      create: (context) =>
-          JoinInputData(), // TODO main.dart의 하위 페이지로 연동 후, ChangeNotifierProvider 코드 삭제
-      child: Scaffold(
+    final joinInputData = Provider.of<JoinInputData>(context);
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
         backgroundColor: backgroundColor,
-        appBar: AppBar(
-          backgroundColor: backgroundColor,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              if (_currentPage > 0) {
-                _pageController.previousPage(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            if (_currentPage > 0) {
+              _pageController.previousPage(
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeInOut,
+              );
+            } else {
+              print('첫 페이지입니다.');
+            }
+          },
+        ),
+        title: const Text('회원가입',
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 18)),
+        actions: [
+          InkWell(
+            onTap: () {
+              if (_currentPage < _totalPage - 1) {
+                _pageController.nextPage(
                   duration: const Duration(milliseconds: 200),
                   curve: Curves.easeInOut,
                 );
               } else {
-                // 첫 페이지에서 뒤로 갈 때의 특별한 동작을 수행
-                print('첫 페이지입니다.');
+                print('마지막 페이지입니다.');
+                UserInfo userInfo = UserInfo(
+                  oauthType: 'apple',
+                  name: joinInputData.name,
+                  nickname: joinInputData.nickname,
+                  birth: joinInputData.birth.replaceAll('.', '-'),
+                  email: joinInputData.email,
+                  deviceToken: '',
+                );
+                List<Baby> babies =
+                    List.generate(joinInputData.birthNames.length, (index) {
+                  return Baby(
+                      name: joinInputData.birthNames[index],
+                      gender: joinInputData.genders[index]);
+                });
+
+                BabyInfo babyInfo = BabyInfo(
+                    dueAt: joinInputData.dueDate.replaceAll('.', '-'),
+                    lastPeriodAt:
+                        joinInputData.laseMensesDate.replaceAll('.', '-'),
+                    babies: babies);
+
+                joinApiService.signUp(userInfo, babyInfo);
               }
             },
+            child: Padding(
+              padding: EdgeInsets.only(right: screenWidth / 19),
+              child: const Text('다음',
+                  style: TextStyle(
+                      color: navOffTextColor, // TODO 텍스트 조건 체크 후, 활성화
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18)),
+            ),
           ),
-          title: const Text('회원가입',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18)),
-          actions: [
-            InkWell(
-              onTap: () {
-                if (_currentPage < _totalPage - 1) {
-                  _pageController.nextPage(
-                    duration: const Duration(milliseconds: 200),
-                    curve: Curves.easeInOut,
-                  );
-                } else {
-                  // 페이지가 끝났을 때 다른 작업 수행
-                  print('마지막 페이지입니다.');
-                }
+        ],
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 43,
+            left: 20,
+            right: 20,
+            child: LinearProgressIndicator(
+              value: (_currentPage + 1) / _totalPage,
+              backgroundColor: mainLineColor,
+              color: primaryColor,
+              borderRadius: BorderRadius.circular(20),
+              minHeight: 6.0,
+            ),
+          ),
+          Positioned.fill(
+            top: 0,
+            left: 0,
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (int page) {
+                setState(() {
+                  _currentPage = page;
+                });
               },
-              child: Padding(
-                padding: EdgeInsets.only(right: screenWidth / 19),
-                child: const Text('다음',
-                    style: TextStyle(
-                        color: navOffTextColor, // TODO 텍스트 조건 체크 후, 활성화
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18)),
-              ),
+              children: const [
+                MomEmailInputScreen(),
+                MomNameBirthInputScreen(),
+                MomNicknameInputScreen(),
+                BabyDuedateInputScreen(),
+                BabyFetalInfoScreen(),
+                BabyGenderScreen(),
+              ],
             ),
-          ],
-        ),
-        body: Stack(
-          children: [
-            Positioned(
-              top: 43,
-              left: 20,
-              child: SizedBox(
-                width: screenWidth - 40,
-                child: LinearProgressIndicator(
-                  value: (_currentPage + 1) / _totalPage,
-                  backgroundColor: mainLineColor,
-                  color: primaryColor,
-                  borderRadius: BorderRadius.circular(20),
-                  minHeight: 6.0,
-                ),
-              ),
-            ),
-            Positioned.fill(
-              top: 0,
-              left: 0,
-              child: PageView(
-                controller: _pageController,
-                onPageChanged: (int page) {
-                  setState(() {
-                    _currentPage = page;
-                  });
-                },
-                children: const [
-                  MomEmailInputScreen(),
-                  MomNameBirthInputScreen(),
-                  MomNicknameInputScreen(),
-                  BabyDuedateInputScreen(),
-                  BabyFetalInfoScreen(),
-                  BabyGenderScreen(),
-                  BabyBirthnameScreen()
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
