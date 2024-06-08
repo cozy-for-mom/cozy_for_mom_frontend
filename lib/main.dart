7582101e91b0dce1eb52f5398e70f6d30c1fda16
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-09 04:59:49 +0900

    Fix: locale bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,12 @@ import 'package:provider/provider.dart';
 import 'package:cozy_for_mom_frontend/model/global_state.dart';
 import 'package:cozy_for_mom_frontend/screen/tab/community/list_modify_state.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  await dotenv.load(
-      fileName:
-          'assets/configs/.env'); // 이 코드를 추가한다.initializeDateFormatting('ko_KR', null).then((_) {
+  await dotenv.load(fileName: 'assets/configs/.env'); // 이 코드를 추가한다.
 
-  await DeviceTokenManager().initialize();
+  // await DeviceTokenManager().initialize();
 
   // runApp() 호출 전 Flutter SDK 초기화
   KakaoSdk.init(
@@ -27,19 +26,21 @@ void main() async {
   );
 
   // 'ko_KR'는 한국어 로케일
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => MyDataModel()),
-        ChangeNotifierProvider(create: (context) => ListModifyState()),
-        ChangeNotifierProvider(create: (context) => JoinInputData()),
-        ChangeNotifierProvider(create: (context) => SupplementApiService()),
-        ChangeNotifierProvider(create: (context) => WeightApiService()),
-        ChangeNotifierProvider(create: (context) => MealApiService()),
-        ChangeNotifierProvider(create: (context) => BloodsugarApiService()),
-        ChangeNotifierProvider(create: (context) => UserApiService())
-      ],
-      child: const App(),
-    ),
-  );
+  initializeDateFormatting('ko_KR', null).then((_) {
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => MyDataModel()),
+          ChangeNotifierProvider(create: (context) => ListModifyState()),
+          ChangeNotifierProvider(create: (context) => JoinInputData()),
+          ChangeNotifierProvider(create: (context) => SupplementApiService()),
+          ChangeNotifierProvider(create: (context) => WeightApiService()),
+          ChangeNotifierProvider(create: (context) => MealApiService()),
+          ChangeNotifierProvider(create: (context) => BloodsugarApiService()),
+          ChangeNotifierProvider(create: (context) => UserApiService())
+        ],
+        child: const App(),
+      ),
+    );
+  });
 }
