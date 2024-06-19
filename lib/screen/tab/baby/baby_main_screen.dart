39661d8d391c1f68d563fe26039421c45bf953cc
ayoung0073 <ayong0310@naver.com>
2024-06-19 21:34:24 +0900

    feat: 태아 메인페이지 스크롤 설정

--- lib/screen/tab/baby/baby_main_screen.dart
+++ lib/screen/tab/baby/baby_main_screen.dart
@@ -38,199 +38,204 @@ class _BabyMainScreenState extends State<BabyMainScreen> {
     double percentage = daysPassed / totalDays;
 
     return Scaffold(
-      body: Stack(
-        children: [
-          const Positioned(
-            top: 0,
-            child: Image(
-                width: 405, // TODO 화면 너비에 맞춘 width로 수정해야함
-                fit: BoxFit.cover,
-                image: AssetImage(
-                    "assets/images/babyhome_dark.png")), // TODO 밤/낮 따라 이미지경로 바꿔줘야 함
-          ),
-          Positioned(
-            top: 66,
-            left: 340,
-            child: IconButton(
-              icon: const Image(
-                width: 30,
-                height: 30,
-                image: AssetImage(
-                  "assets/images/icons/mypage.png",
+      body: SingleChildScrollView(
+        child: Container(
+        height: MediaQuery.of(context).size.height * 1.1,
+          child: Stack(
+            children: [
+              const Positioned(
+                top: 0,
+                child: Image(
+                    width: 405, // TODO 화면 너비에 맞춘 width로 수정해야함
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                        "assets/images/babyhome_dark.png")), // TODO 밤/낮 따라 이미지경로 바꿔줘야 함
+              ),
+              Positioned(
+                top: 66,
+                left: 340,
+                child: IconButton(
+                  icon: const Image(
+                    width: 30,
+                    height: 30,
+                    image: AssetImage(
+                      "assets/images/icons/mypage.png",
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const MyPage()));
+                  },
                 ),
               ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const MyPage()));
-              },
-            ),
-          ),
-          Positioned(
-              top: 145,
-              left: 0,
-              right: 0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(baby.name,
-                      style: const TextStyle(
-                          color: mainTextColor,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 26)),
-                  const SizedBox(height: 5),
-                  const Text('임신 12주차 1일째',
-                      style: TextStyle(
-                          color: Color(0xff9D8DFF),
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16)),
-                ],
-              )),
-          const Positioned(
-            top: 278,
-            left: 106,
-            child: Image(
-                width: 187,
-                height: 145,
-                image: AssetImage("assets/images/baby2_1.png")),
-          ),
-          const Positioned(
-            top: 291,
-            left: 120,
-            child: Image(
-                width: 167,
-                height: 125,
-                image: AssetImage("assets/images/baby2_2.png")),
-          ),
-          const Positioned(
-            top: 355.52,
-            left: 114,
-            child: Image(
-                width: 66,
-                height: 34.67,
-                image: AssetImage("assets/images/umbilical_cord.png")),
-          ),
-          Positioned(
-            top: 433,
-            left: 0,
-            right: 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "${baby.name}와 만나는 날",
-                      style: const TextStyle(
-                          color: mainTextColor,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16),
-                    ),
-                    Text(' D-${difference.inDays}', // TODO 밤/낮 따라 색상 바꿔줘야 함
-                        style: const TextStyle(
-                            color: babyNightBar,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 16)),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Container(
-                  width: 313,
-                  height: 12,
-                  decoration: BoxDecoration(
-                    color: lineTwoColor, // 전체 배경색
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: FractionallySizedBox(
-                    widthFactor: percentage,
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: babyNightBar, // TODO 밤/낮 따라 색상 바꿔줘야 함
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 501.52,
-            left: 20,
-            child: SizedBox(
-              width: 350,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const BabyGrowthReportListScreen()));
-                      },
-                      child: const CustomButton(
-                          text: '성장 보고서',
-                          imagePath: "assets/images/icons/diary.png"),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MyCozylog(),
-                          ),
-                        );
-                      },
-                      child: const CustomButton(
-                        text: '코지로그',
-                        imagePath: "assets/images/icons/cozylog.png",
-                      ),
-                    ),
-                  ]),
-            ),
-          ),
-          Positioned(
-              top: 613.52,
-              left: 20,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('성장일지를 기록해요',
-                      style: TextStyle(
-                          color: mainTextColor,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 18)),
-                  const SizedBox(height: 18),
-                  Container(
-                    width: 350,
-                    height: 100,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    decoration: BoxDecoration(
-                        color: babyNightBar,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Positioned(
+                  top: 145,
+                  left: 0,
+                  right: 0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(baby.name,
+                          style: const TextStyle(
+                              color: mainTextColor,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 26)),
+                      const SizedBox(height: 5),
+                      const Text('임신 12주차 1일째',
+                          style: TextStyle(
+                              color: Color(0xff9D8DFF),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16)),
+                    ],
+                  )),
+              const Positioned(
+                top: 278,
+                left: 106,
+                child: Image(
+                    width: 187,
+                    height: 145,
+                    image: AssetImage("assets/images/baby2_1.png")),
+              ),
+              const Positioned(
+                top: 291,
+                left: 120,
+                child: Image(
+                    width: 167,
+                    height: 125,
+                    image: AssetImage("assets/images/baby2_2.png")),
+              ),
+              const Positioned(
+                top: 355.52,
+                left: 114,
+                child: Image(
+                    width: 66,
+                    height: 34.67,
+                    image: AssetImage("assets/images/umbilical_cord.png")),
+              ),
+              Positioned(
+                top: 433,
+                left: 0,
+                right: 0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text('오늘은 얼마나 자랐을까?',
-                            style: TextStyle(
-                                color: Colors.white,
+                        Text(
+                          "${baby.name}와 만나는 날",
+                          style: const TextStyle(
+                              color: mainTextColor,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16),
+                        ),
+                        Text(' D-${difference.inDays}', // TODO 밤/낮 따라 색상 바꿔줘야 함
+                            style: const TextStyle(
+                                color: babyNightBar,
                                 fontWeight: FontWeight.w700,
                                 fontSize: 16)),
-                        Image(
-                            image: AssetImage(
-                                "assets/images/icons/diary_cozylog.png"),
-                            width: 78.44,
-                            height: 53.27),
                       ],
                     ),
-                  )
-                ],
-              ))
-        ],
+                    const SizedBox(height: 10),
+                    Container(
+                      width: 313,
+                      height: 12,
+                      decoration: BoxDecoration(
+                        color: lineTwoColor, // 전체 배경색
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: FractionallySizedBox(
+                        widthFactor: percentage,
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          height: 20,
+                          decoration: BoxDecoration(
+                            color: babyNightBar, // TODO 밤/낮 따라 색상 바꿔줘야 함
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 501.52,
+                left: 20,
+                child: SizedBox(
+                  width: 350,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const BabyGrowthReportListScreen()));
+                          },
+                          child: const CustomButton(
+                              text: '성장 보고서',
+                              imagePath: "assets/images/icons/diary.png"),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MyCozylog(),
+                              ),
+                            );
+                          },
+                          child: const CustomButton(
+                            text: '코지로그',
+                            imagePath: "assets/images/icons/cozylog.png",
+                          ),
+                        ),
+                      ]),
+                ),
+              ),
+              Positioned(
+                  top: 613.52,
+                  left: 20,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('성장일지를 기록해요',
+                          style: TextStyle(
+                              color: mainTextColor,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18)),
+                      const SizedBox(height: 18),
+                      Container(
+                        width: 350,
+                        height: 100,
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        decoration: BoxDecoration(
+                            color: babyNightBar,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('오늘은 얼마나 자랐을까?',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 16)),
+                            Image(
+                                image: AssetImage(
+                                    "assets/images/icons/diary_cozylog.png"),
+                                width: 78.44,
+                                height: 53.27),
+                          ],
+                        ),
+                      )
+                    ],
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
