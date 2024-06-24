adcbb1dacabace213df57e1899c40a1ebfab5847
ck <70942824+ck-pp@users.noreply.github.com>
2024-06-25 04:38:51 +0900

    🐛 Fix: 회원가입 성공 시, 메인페이지로 넘어가도록 수정

--- lib/service/user/join_api_service.dart
+++ lib/service/user/join_api_service.dart
@@ -3,10 +3,16 @@ import 'dart:convert';
 import 'package:cozy_for_mom_frontend/service/base_api.dart';
 import 'package:cozy_for_mom_frontend/service/base_headers.dart';
 import 'package:cozy_for_mom_frontend/model/user_join_model.dart';
+import 'package:cozy_for_mom_frontend/service/user/oauth_api_service.dart';
+import 'package:cozy_for_mom_frontend/service/user/token_manager.dart'
+    as TokenManager;
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class JoinApiService extends ChangeNotifier {
+  final tokenManager = TokenManager.TokenManager();
+
   Future<Map<String, dynamic>> signUp(
       UserInfo userInfo, BabyInfo babyInfo) async {
     final url = Uri.parse('$baseUrl/user/signup');
@@ -17,8 +23,12 @@ class JoinApiService extends ChangeNotifier {
     };
     final Response response =
         await post(url, headers: headers, body: jsonEncode(data));
-
     if (response.statusCode == 201) {
+      final accessToken =
+          (response.headers['authorization'] as String).split(' ')[1];
+      tokenManager.setToken(accessToken);
+      final decoded = JwtDecoder.decode(accessToken);
+      print(UserType.findByString(decoded['info']['role']));
       return jsonDecode(utf8.decode(response.bodyBytes));
     } else {
       throw Exception('회원가입을 실패하였습니다.');
