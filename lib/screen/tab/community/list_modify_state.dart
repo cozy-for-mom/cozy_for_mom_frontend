1dab1e77bd95b525b6debf3be996fe8098d2b834
ayoung0073 <ayong0310@naver.com>
2024-06-30 15:49:29 +0900

    fix: selectedCount 계산 로직 생략, _selectedIds로 판단하도록 변경

--- lib/screen/tab/community/list_modify_state.dart
+++ lib/screen/tab/community/list_modify_state.dart
@@ -2,8 +2,7 @@ import 'package:cozy_for_mom_frontend/screen/tab/cozylog/cozylog_model.dart';
 import 'package:flutter/material.dart';
 
 class ListModifyState extends ChangeNotifier {
-  int _selectedCount = 0;
-  int get selectedCount => _selectedCount;
+  int get selectedCount => _selectedIds.length;
 
   List<int> _selectedIds = [];
   List<int> get selectedIds => _selectedIds;
@@ -15,10 +14,8 @@ class ListModifyState extends ChangeNotifier {
   void toggleSelected(int id) {
     if (_selectedIds.contains(id)) {
       _selectedIds.remove(id);
-      _selectedCount--;
     } else {
       _selectedIds.add(id);
-      _selectedCount++;
     }
 
     notifyListeners();
@@ -26,13 +23,11 @@ class ListModifyState extends ChangeNotifier {
 
   void setAllSelected(List<CozyLogForList> cozyLogs) {
     _selectedIds = cozyLogs.map((cozyLog) => cozyLog.cozyLogId).toList();
-    _selectedCount = cozyLogs.length;
     notifyListeners();
   }
 
   void clearSelection() {
     _selectedIds.clear();
-    _selectedCount = 0;
     notifyListeners();
   }
 }
